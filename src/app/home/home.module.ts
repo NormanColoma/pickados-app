c76b1ced9bc620bb94d234117beece5ff9536b1b
Jose Antonio Rubio <josearuol@gmail.com>
2017-05-08 01:29:00 +0200

    Added base intro page to start working and some styles and images

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { CommonModule } from '@angular/common';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
     declarations: [
         HomeComponent
     ],
     imports:[
-        CommonModule
+        CommonModule,
+         RouterModule
     ],
     exports: [
         HomeComponent
