b1266ea450faccd351087557ec80b1703c696264
Norman <ua.norman@gmail.com>
2017-07-10 19:26:37 +0200

    no message

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'login-component',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+    styleUrls: [
+        './login.component.css'
+    ]
 })
 
 export class LoginComponent {}
